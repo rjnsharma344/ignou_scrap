a451aaf7faff6679a82b6a57f4b2229973dd339c
Arjun sharmaa I <rjnsharma344@gmail.com>
2018-05-15 18:00:25 +0530

    Debugging for beggining levels removed

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const options = {	uri:	'http://egyankosh.ac.in/handle/123456789/3246',
-	transform: function(body) {return cheerio.load(body);}
-};
 
 l2('http://egyankosh.ac.in/handle/123456789/3246');
 
 function l2(l2in){
-//if(l2in=='http://egyankosh.ac.in'){console.log('Completed');}
-//console.log(l2in);
-// Since there will be no list-group-item-heading class on last file there is no need for special terminate check
 	var opt={	url: l2in,
 			transform: function(body) {return cheerio.load(body);}
 	};
