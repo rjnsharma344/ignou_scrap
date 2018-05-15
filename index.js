1fdddc40e737db2a63e17254e3689e2a31d11184
Arjun sharmaa I <rjnsharma344@gmail.com>
2018-05-15 16:35:35 +0530

    Old initiator removed

--- index.js
+++ index.js
@@ -7,29 +7,6 @@ const options = {	uri:	'http://egyankosh.ac.in/handle/123456789/3246',
 var arr = new Array()
 l2('http://egyankosh.ac.in/handle/123456789/3246');
 
-/*
-rp(options)
-	.then(($)=>{
-
-			$('.list-group-item-heading').children('a').each(function(i,elm){
-					//console.log('http://egyankosh.ac.in'+$(this).attr('href'))
-					// Culrs basic page of PubAd from IGNOU website and forms an array having all the links to future levels
-					arr.push('http://egyankosh.ac.in'+$(this).attr('href'));
-					l2(arr[i]);
-					//console.log(arr);
-					if(i==$('.list-group-item-heading').children('a').length-1)
-					{
-					console.log($('.list-group-item-heading').children('a').length +'-'+ i);
-					console.log('Finished')
-					console.log(arr);
-					}
-					})
-			})
-.catch((err)=>{
-		console.log(err);
-		});
-
-*/
 
 function l2(l2in){
 //if(l2in=='http://egyankosh.ac.in'){console.log('Completed');}
