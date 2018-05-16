5d7beb6b9431d75051a53572fab53efc42541fa3
Arjun sharmaa I <rjnsharma344@gmail.com>
2018-05-16 13:03:31 +0530

    Single function for checking and creating directory if not found

--- dummy.js
+++ dummy.js
@@ -3,39 +3,43 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 path='down/';
 func('http://egyankosh.ac.in/handle/123456789/3246');
+checkdir(path);
+
+function checkdir(path_to_dir){
+
+	if(fs.existsSync(path_to_dir)){//console.log("Path exists");
+	}
+	else{
+		console.log(path_to_dir+"Path doesn't exist.. Creating");
+		fs.mkdirSync(path_to_dir);
+	}
+
+}
 
 function func(inp,l1,l2,l3){
+	if(l1&&l2&&l3){
+		l1=l1.trim();
 
-if(fs.existsSync(path)){//console.log("Path exists");
-}
-else{
-console.log("Path doesn't exist.. Creating");
-fs.mkdirSync(path);
-}
-
-if(l1&&l2&&l3){
-l1=l1.trim();
-
-if(fs.existsSync(path+l1)){//console.log("Path exists");
-}
-else{
-console.log("Path doesn't exist.. Creating");
-fs.mkdirSync(path+l1);
-}
+		if(fs.existsSync(path+l1)){//console.log("Path exists");
+		}
+		else{
+			console.log("Path doesn't exist.. Creating");
+			fs.mkdirSync(path+l1);
+		}
 
 
-l2=l2.trim();
+		l2=l2.trim();
 
-if(fs.existsSync(path+l1+'/'+l2)){//console.log("Path exists");
-}
-else{
-console.log("Path doesn't exist.. Creating");
-fs.mkdirSync(path+l1+'/'+l2);
-}
+		if(fs.existsSync(path+l1+'/'+l2)){//console.log("Path exists");
+		}
+		else{
+			console.log("Path doesn't exist.. Creating");
+			fs.mkdirSync(path+l1+'/'+l2);
+		}
 
-l3=l3.trim();
-//console.log(l1+' '+l2+' '+l3);
-}
+		l3=l3.trim();
+		//console.log(l1+' '+l2+' '+l3);
+	}
 	var opt={	url: inp,
 		transform: function(body) {return cheerio.load(body);}
 	};
@@ -44,10 +48,10 @@ l3=l3.trim();
 
 				$('.list-group-item-heading').children('a').each(function(i,elm){
 
-if(!l1){func('http://egyankosh.ac.in'+$(this).attr('href'),$(this).text());}
-if(l1){func('http://egyankosh.ac.in'+$(this).attr('href'),l1,$(this).text());}
+						if(!l1){func('http://egyankosh.ac.in'+$(this).attr('href'),$(this).text());}
+						if(l1){func('http://egyankosh.ac.in'+$(this).attr('href'),l1,$(this).text());}
 
-//						console.log($(this).text())
+						//						console.log($(this).text())
 						});
 
 				if($('strong'!=null)){
@@ -60,12 +64,12 @@ if(l1){func('http://egyankosh.ac.in'+$(this).attr('href'),l1,$(this).text());}
 				}
 
 				if($('meta[name="citation_pdf_url"]').length==1){
-//				console.log(l1+'/'+l2+'/'+l3+'.pdf');
-				down_path=path+l1+'/'+l2+'/'+l3+'.pdf';
-				console.log(down_path);
-				console.log($('meta[name="citation_pdf_url"]').attr('content'));
+				//				console.log(l1+'/'+l2+'/'+l3+'.pdf');
+					down_path=path+l1+'/'+l2+'/'+l3+'.pdf';
+					console.log(down_path);
+					console.log($('meta[name="citation_pdf_url"]').attr('content'));
 				}
 
-				});
+		});
 }
 
