2ec791a6176527fbb7565bd31870f6a58d605594
Arjun sharmaa I <rjnsharma344@gmail.com>
2018-05-16 13:17:12 +0530

    Trim functions performed at the calling function

--- dummy.js
+++ dummy.js
@@ -6,23 +6,22 @@ func('http://egyankosh.ac.in/handle/123456789/3246');
 checkdir(path);
 
 function checkdir(path_to_dir){
-
 	if(fs.existsSync(path_to_dir)){//console.log("Path exists");
 	}
 	else{
 		console.log(path_to_dir+"Path doesn't exist.. Creating");
 		fs.mkdirSync(path_to_dir);
 	}
-
 }
 
 function func(inp,l1,l2,l3){
+		
 	if(l1&&l2&&l3){
-		l1=l1.trim();
+//		l1=l1.trim();
 		checkdir(path+l1);
-		l2=l2.trim();
+//		l2=l2.trim();
 		checkdir(path+l1+'/'+l2);
-		l3=l3.trim();
+//		l3=l3.trim();
 
 		//console.log(l1+' '+l2+' '+l3);
 	}
@@ -34,8 +33,8 @@ function func(inp,l1,l2,l3){
 
 				$('.list-group-item-heading').children('a').each(function(i,elm){
 
-						if(!l1){func('http://egyankosh.ac.in'+$(this).attr('href'),$(this).text());}
-						if(l1){func('http://egyankosh.ac.in'+$(this).attr('href'),l1,$(this).text());}
+						if(!l1){func('http://egyankosh.ac.in'+$(this).attr('href'),$(this).text().trim());}
+						if(l1){func('http://egyankosh.ac.in'+$(this).attr('href'),l1,$(this).text().trim());}
 
 						//						console.log($(this).text())
 						});
@@ -43,7 +42,7 @@ function func(inp,l1,l2,l3){
 				if($('strong'!=null)){
 				$('strong').each(function(i,elm){
 						if($(this).children('a').length!=0){				
-						func('http://egyankosh.ac.in'+$(this).children('a').attr('href'),l1,l2,$(this).children('a').text());
+						func('http://egyankosh.ac.in'+$(this).children('a').attr('href'),l1,l2,$(this).children('a').text().trim());
 						}
 						});
 
